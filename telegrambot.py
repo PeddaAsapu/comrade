d6917122f4158bb18ec06cb43fe507d6cd6a83ca
Pedda Babu Asapu <pedda.asapu@datacom.com.au>
2023-01-11 22:03:46 +1100

    Improvements - 1

--- telegrambot.py
+++ telegrambot.py
@@ -14,7 +14,23 @@ API_KEY = '5969605735:AAHRetW-aJBlO05QyhcXgHjpFNuDc57z_zY'
 print("Bot started")
 
 def get_trip_details(update) :
-    update.message.reply_text('Select your means of transportation: \n\n' +'/Air \n \n' +'/Rail')
+    text = str(update.message.text).lower()
+    user_id = update.message.chat.id
+    user_trips = Trip.objects.filter(user=user_id)
+    
+    for trip in user_trips:
+        message = "Your Trip details are below: \n"
+        message += "="*19
+        message += f"\nMode of Transport : {trip.mode_of_transport} \n"
+        if trip.mode_of_transport.name == 'Flight':
+            message += f"Airlines: {trip.vehicle.provider} \n"
+            message += f"Flight Number: {trip.vehicle} \n"
+        message += f"Date: {trip.travel_date_time.date()} \n"
+        message += f"Time of Flight: {trip.travel_date_time.time().strftime('%H:%M')} \n"
+        message += f"From: {trip.source_location} \n"
+        message += f"To: {trip.destination_location} \n"
+    return message
+    #update.message.reply_text('Select your means of transportation: \n\n' +'/Air \n \n' +'/Rail')
 
 
 def existing_user(id):
@@ -24,10 +40,6 @@ def existing_user(id):
     return False 
 
 def create_user(update) :
-    print("Update details")
-    print("="*100)
-    print(update)
-    print("="*100)
     first_name = str(update.message.chat.first_name)
     last_name = str(update.message.chat.last_name)
     user_id = str(update.message.chat.id)
@@ -40,27 +52,43 @@ def create_user(update) :
     telegram_users = TelegramUsers(user_id=user_id,first_name=first_name, last_name=last_name, is_bot=is_bot)
     telegram_users.save()
 
+def exit_command(update):
+    update.message.reply_text("Thank You. See you again :) ")   
+
 def handle_message(update, context):
     text = str(update.message.text).lower()
-    print("update : ", update)
-    print("text Entered : ", text)
     first_name = str(update.message.chat.first_name).capitalize()
     user_id = update.message.chat.id
-    if not existing_user(user_id):
+    if not existing_user(user_id):    
         create_user(update)
-    print(f"User {first_name} Already exists ")
-    update.message.reply_text(f'Hi {first_name}. Hearty Welcome from team Comrade.')
-    update.message.reply_text('Are you ready to enter your trip details ? \n\n' +'/Yes \n \n' +'/No')
-    if text == '/yes' :
-        get_trip_details(update)
-    else :
-        update.message.reply_text('Choose \n\n' +'/Ready  \n When you are ready ')
+    print("text ", text )
+    if text not in ['/yes','/no','/explore','/exit'] :
+        update.message.reply_text(f'Hi {first_name}. Welcome from team Comrade.')
+        update.message.reply_text('Glad that we have you in our System Already!!')
+        update.message.reply_text('Would you like to see your existing Trip Details ? \n' +'/Yes \n' +'/No')
+    elif text == '/yes' :
+        message = get_trip_details(update)
+        update.message.reply_text(message)
+        update.message.reply_text('Explore other options ? \n' +'/Explore \n' +'/Exit')
+    elif text == '/no' or  text == '/explore':
+        update.message.reply_text("Choose the Other Options from below: \n")
+        update.message.reply_text('/Edit  - Edit Trip Details \n' +'/Find - Finds your travel mate \n' + '/Help \n' + '/Exit')
+    
+    elif text == '/exit' :
+        exit_command(update)
+    
+    #update.message.reply_text('Are you ready to enter your trip details ? \n\n' +'/Yes \n \n' +'/No')
+    #if text == '/yes' :
+    #    get_trip_details(update)
+    #else :
+    #    update.message.reply_text('Choose \n\n' +'/Ready  \n When you are ready ')
     
     
 if __name__ == '__main__' :
     updater = Updater(API_KEY, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(MessageHandler(Filters.text, handle_message))
+    dp.add_handler(CommandHandler("My_Trip_Details", handle_message))
     dp.add_handler(CommandHandler("Yes", get_trip_details))
-    updater.start_polling(1.0)
+    updater.start_polling()
     updater.idle()
